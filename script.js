6fb45bcbef216bb1139c4e304e917beb6d35bc92
pukuzo <44659059+pukuzo@users.noreply.github.com>
2024-07-22 12:16:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -31,8 +31,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const yesterdayStr = formatDate(yesterday);
 
     // 複数のエラーチェックを行う
-    if (!keyParam || !startParam || !endParam || !breakParam || !lateNightBreakParam || !nameParam) {
-        errorMessage = 'すべてのパラメータが揃っていません。';
+    if (!keyParam) {
+        errorMessage = 'keyパラメータがありません。';
+    } else if (keyParam.length !== 10) {
+        errorMessage = 'keyパラメータの長さが10文字ではありません。';
+    } else if (!/^[a-zA-Z0-9]+$/.test(keyParam)) {
+        errorMessage = 'keyパラメータには英数字のみを使用してください。';
     } else {
         // keyパラメータの先頭8桁を取得
         const datePart = keyParam.slice(0, 8);
